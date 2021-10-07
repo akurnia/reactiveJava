65617201fc6667b0aae0c61169cf01593c8ffcc9
Arief Kurnia <arief.kurnia@pegipegi.com>
2021-10-07 10:45:52 +0700

    create class test

---
+++ src/test/java/com/learnreactivespring/repository/ItemReactiveRepositorytest.java
@@ -0,0 +1,25 @@
+package com.learnreactivespring.repository;
+
+import com.learnreactivespring.document.Item;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class ItemReactiveRepositorytest {
+
+
+
+}
