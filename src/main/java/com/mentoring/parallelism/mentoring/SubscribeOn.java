c4bd6e3143076554c86c119160f57329bf119e9a
Arief Kurnia <arief.kurnia@pegipegi.com>
2021-11-16 16:55:32 +0700

    subscribeOn-boundedElastic test with runnable

--- src/main/java/com/mentoring/parallelism/mentoring/SubscribeOn.java
+++ src/main/java/com/mentoring/parallelism/mentoring/SubscribeOn.java
@@ -11,12 +11,15 @@ public class SubscribeOn {
                     objectFluxSink.next(1);
                 })
                 .doOnNext(i->printThreadName("next "+i));
-        flux
+        Runnable runnable = ()-> flux
                 .doFirst(() -> printThreadName("first2"))
                 .subscribeOn(Schedulers.boundedElastic())
                 .doFirst(() -> printThreadName("first1"))
                 .subscribe(v -> printThreadName("sub "+v));
 
+        for (int i=0; i<2; i++ ){
+            new Thread(runnable).start();
+        }
 
 
         try {
